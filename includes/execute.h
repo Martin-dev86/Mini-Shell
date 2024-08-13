ddc22cb1d29695fd9922726b30cdc9cbd7af1667
Carlos Garcia Martin <cagarci2@c3r5s5.42malaga.com>
2024-08-13 13:39:31 +0200

    add execute.h

--- includes/execute.h
+++ includes/execute.h
@@ -6,10 +6,18 @@
 /*   By: cagarci2 <cagarci2@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 13:14:52 by cagarci2          #+#    #+#             */
-/*   Updated: 2024/08/13 13:19:27 by cagarci2         ###   ########.fr       */
+/*   Updated: 2024/08/13 13:32:55 by cagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #DEFINE READ_END = 0;
 #DEFINE WRITE_END = 1;
+
+typedef struct s_execute
+{
+    pid_t   pid;
+    int     fd_in;
+    int     fd_out;
+    int     fd[2];
+}
