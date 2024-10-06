d91b9d427a5c23bb5e05007ecd0980ffab9374b3
jeandrad1 <jesusandradeperez@gmail.com>
2024-10-06 18:43:39 +0200

    Beginning the reestructuration of every function because of the freaking norminette

--- Parse/ast_creator.c
+++ Parse/ast_creator.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ast_creator.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cagarci2 <cagarci2@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:57:35 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/10/03 22:53:31 by cagarci2         ###   ########.fr       */
+/*   Updated: 2024/10/06 18:42:07 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,93 +15,87 @@
 // Function to create a new AST node
 t_node	*create_ast_node(t_token *operation)
 {
-	t_node	*node;
+    t_node	*node;
 
-	node = (t_node *)malloc(sizeof(t_node));
-	if (!node)
-	{
-		perror("malloc failed");
-		exit(EXIT_FAILURE);
-	}
-	node->operation = operation;
-	node->left = NULL;
-	node->right = NULL;
-	node->n_childs = 0;
-	return (node);
+    node = (t_node *)malloc(sizeof(t_node));
+    if (!node)
+    {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    node->operation = operation;
+    node->left = NULL;
+    node->right = NULL;
+    node->n_childs = 0;
+    return (node);
 }
 
 // Function to find the highest-priority token in the token list
 t_list_token	*find_highest_priority_token(t_list_token *start,
-		t_list_token *end)
+        t_list_token *end)
 {
-	t_list_token	*current;
-	t_list_token	*highest_priority_token;
+    t_list_token	*current;
+    t_list_token	*highest_priority_token;
 
-	current = start;
-	highest_priority_token = start;
-	while (current != end->next)
-	{
-		if (current->content->priority < highest_priority_token->content->priority)
-		{
-			highest_priority_token = current;
-		}
-		current = current->next;
-	}
-	return (highest_priority_token);
+    current = start;
+    highest_priority_token = start;
+    while (current != end->next)
+    {
+        if (current->content->priority < \
+        highest_priority_token->content->priority)
+        {
+            highest_priority_token = current;
+        }
+        current = current->next;
+    }
+    return (highest_priority_token);
+}
+
+// Function to handle different token types
+void	handle_token_type(t_node *node, t_list_token *highest_priority_token, t_list_token *start, t_list_token *end)
+{
+    if (highest_priority_token->content->type == PIPE)
+    {
+        if (highest_priority_token->prev != NULL)
+            node->left = ast_creator(start, highest_priority_token->prev);
+        if (highest_priority_token->next != NULL)
+            node->right = ast_creator(highest_priority_token->next, end);
+    }
+    else if (highest_priority_token->content->type == REDIR_L
+        || highest_priority_token->content->type == REDIR_R
+        || highest_priority_token->content->type == APPEND
+        || highest_priority_token->content->type == HEREDOC)
+    {
+        if (highest_priority_token->prev != NULL)
+            node->left = ast_creator(start, highest_priority_token->prev);
+        if (highest_priority_token->next != NULL)
+        {
+            if (highest_priority_token->content->type == HEREDOC)
+                node->right = create_ast_node(highest_priority_token->next->content);
+            else
+                node->right = create_ast_node(highest_priority_token->next->content);
+        }
+    }
+    else if (highest_priority_token->content->type == CMD
+        || highest_priority_token->content->type == ARG)
+    {
+        if (highest_priority_token->next != NULL)
+            node->right = ast_creator(highest_priority_token->next, end);
+        if (highest_priority_token->prev != NULL)
+            node->left = ast_creator(start, highest_priority_token->prev);
+    }
 }
 
 // Recursive function to build the AST
 t_node	*ast_creator(t_list_token *start, t_list_token *end)
 {
-	if (start == NULL || start == end->next)
-		return (NULL);
-	// Find the token with the highest priority (pipes first, then redirections)
-	t_list_token *highest_priority_token = find_highest_priority_token(start,
-			end);
-	// Create a node for the highest-priority token
-	t_node *node = create_ast_node(highest_priority_token->content);
-	// If the token is a pipe, its children are commands or other pipes
-	if (highest_priority_token->content->type == PIPE)
-	{
-		if (highest_priority_token->prev != NULL)
-			node->left = ast_creator(start, highest_priority_token->prev);
-		if (highest_priority_token->next != NULL)
-			node->right = ast_creator(highest_priority_token->next, end);
-	}
-	// If the token is a redirection, attach it to the appropriate command node
-	else if (highest_priority_token->content->type == REDIR_L
-		|| highest_priority_token->content->type == REDIR_R
-		|| highest_priority_token->content->type == APPEND
-		|| highest_priority_token->content->type == HEREDOC)
-	{
-		// Redirection affects the right side (file), so left is the command,
-		//	right is the file
-		if (highest_priority_token->prev != NULL)
-		{
-			//printf("Creating left node\n");
-			node->left = ast_creator(start, highest_priority_token->prev);
-			//node->left->right = node; // Attach the redirection to the command
-		}
-		if (highest_priority_token->next != NULL)
-		{
-			//printf("Creating right node\n");
-			if (highest_priority_token->content->type == HEREDOC)
-				node->right = create_ast_node(highest_priority_token->next->content);
-			else
-				node->right = create_ast_node(highest_priority_token->next->content);
-			
-		}
-	}
-	// If it's a command or argument,
-	//	there may be more tokens to process in a sequential order
-	else if (highest_priority_token->content->type == CMD
-		|| highest_priority_token->content->type == ARG)
-	{
-		if (highest_priority_token->next != NULL )
-			node->right = ast_creator(highest_priority_token->next, end);
-
-		if (highest_priority_token->prev != NULL)
-			node->left = ast_creator(start, highest_priority_token->prev);
-	}
-	return (node);
+    if (start == NULL || start == end->next)
+        return (NULL);
+    // Find the token with the highest priority (pipes first, then redirections)
+    t_list_token *highest_priority_token = find_highest_priority_token(start, end);
+    // Create a node for the highest-priority token
+    t_node *node = create_ast_node(highest_priority_token->content);
+    // Handle the token type
+    handle_token_type(node, highest_priority_token, start, end);
+    return (node);
 }
