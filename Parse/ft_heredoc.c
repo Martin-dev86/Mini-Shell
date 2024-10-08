e60d0cf32783507f566fa33f46ee470c6a39b4f6
jeandrad1 <jesusandradeperez@gmail.com>
2024-10-08 19:45:14 +0200

    heardoc passes norm now

--- Parse/ft_heredoc.c
+++ Parse/ft_heredoc.c
@@ -6,13 +6,13 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 19:00:52 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/10/08 19:36:52 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/10/08 19:44:57 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	open_temp_file(char **temp_filename, int *temp_fd)
+void	create_temp_file(char **temp_filename, int *temp_fd)
 {
 	*temp_filename = strdup("/tmp/heredocXXXXXX");
 	if (!(*temp_filename))
@@ -25,56 +25,49 @@ void	open_temp_file(char **temp_filename, int *temp_fd)
 	}
 }
 
-void	read_heredoc_content(int temp_fd, char *delimiter)
+void	process_heredoc_content(t_node *node, int temp_fd)
 {
 	char	*line;
 
-	line = NULL;
-	printf("Enter heredoc (end with '%s'):\n", delimiter);
-	while (1)
+	printf("Enter heredoc (end with '%s'):\n", node->right->operation->read);
+	line = readline("> ");
+	while (line != NULL)
 	{
-		line = readline("> ");
-		if (!line || strcmp(line, delimiter) == 0)
+		if (strcmp(line, node->right->operation->read) == 0)
 			break ;
 		write(temp_fd, line, strlen(line));
 		write(temp_fd, "\n", 1);
 		free(line);
+		line = readline("> ");
 	}
 	if (line == NULL)
-		printf("Warning: heredoc ended with EOF (not '%s')\n", delimiter);
+		printf("Warning: heredoc ended with EOF (not '%s')\n",
+			node->right->operation->read);
 	free(line);
-}
-
-void	set_heredoc_fd(t_son *son, char *temp_filename)
-{
-	son->fd_heredoc = open(temp_filename, O_RDONLY);
-	if (son->fd_heredoc == -1)
-	{
-		unlink(temp_filename);
-		free(temp_filename);
-		ft_exit("open failed", EXIT_FAILURE);
-	}
-	unlink(temp_filename);
-	free(temp_filename);
-}
-
-void	handle_heredoc_node(t_node *node, t_son *son)
-{
-	int		temp_fd;
-	char	*temp_filename;
-
-	open_temp_file(&temp_filename, &temp_fd);
-	read_heredoc_content(temp_fd, node->right->operation->read);
-	close(temp_fd);
-	set_heredoc_fd(son, temp_filename);
 }
 
 void	handle_heredoc(t_node *node, t_son *son)
 {
+	int		temp_fd;
+	char	*temp_filename;
+
 	if (node == NULL)
 		return ;
 	handle_heredoc(node->left, son);
 	handle_heredoc(node->right, son);
 	if (node->operation->type == HEREDOC)
-		handle_heredoc_node(node, son);
+	{
+		create_temp_file(&temp_filename, &temp_fd);
+		process_heredoc_content(node, temp_fd);
+		close(temp_fd);
+		son->fd_heredoc = open(temp_filename, O_RDONLY);
+		if (son->fd_heredoc == -1)
+		{
+			unlink(temp_filename);
+			free(temp_filename);
+			ft_exit("open failed", EXIT_FAILURE);
+		}
+		unlink(temp_filename);
+		free(temp_filename);
+	}
 }
