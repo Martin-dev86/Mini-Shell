57f494e437dc8eeb2fcc52d2349c0e08359a1b3d
jeandrad1 <jesusandradeperez@gmail.com>
2024-10-08 20:14:57 +0200

    changed main. Signals I think maybe trouble

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cagarci2 <cagarci2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:04:06 by cagarci2          #+#    #+#             */
-/*   Updated: 2024/10/04 19:07:02 by cagarci2         ###   ########.fr       */
+/*   Updated: 2024/10/08 20:14:47 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,12 @@
 
 int	prompt_active;
 
-int	main(int argc, char **argv, char **env)
+void	shell_loop(t_list_shellenv *result, t_son *son)
 {
-	t_token				*token;
-	t_son				*son;
-	t_node				*node;
-	t_list_shellenv		*result;
+	t_token	*token;
+	t_node	*node;
 
-	(void)argc;
-	(void)argv;
-	ft_baby_yoda_banner();
-	setup_signals();
-	rl_catch_signals = 0;
-	result = (t_list_shellenv *)malloc(sizeof(t_list_shellenv));
-	son = malloc(sizeof(t_son));
-	if (son == NULL)
-		ft_exit("Failed to allocate memory for son", EXIT_FAILURE);
 	node = ft_calloc(0, sizeof(t_node));
-	if (son == NULL)
-		ft_exit("Failed to crate node", EXIT_FAILURE);
-	result->env = env_parse(env);
 	while (1)
 	{
 		prompt_active = 0;
@@ -49,9 +35,76 @@ int	main(int argc, char **argv, char **env)
 		if (token->read == NULL)
 			break ;
 		node = main_parser(result, token, son);
+		if (node == NULL)
+			continue ;
 		prompt_active = 1;
 		execute(son, result, node);
 	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_son				*son;
+	t_list_shellenv		*result;
+
+	(void)argc;
+	(void)argv;
+	ft_baby_yoda_banner();
+	setup_signals();
+	rl_catch_signals = 0;
+	result = (t_list_shellenv *)malloc(sizeof(t_list_shellenv));
+	son = malloc(sizeof(t_son));
+	if (son == NULL)
+		ft_exit("Failed to allocate memory for son", EXIT_FAILURE);
+	if (son == NULL)
+		ft_exit("Failed to crate node", EXIT_FAILURE);
+	result->env = env_parse(env);
+	shell_loop(result, son);
 	//ft_free(result, node, NULL, son);
 	return (son->code);
 }
+
+// int	main(int argc, char **argv, char **env)
+// {
+// 	t_token				*token;
+// 	t_son				*son;
+// 	t_node				*node;
+// 	t_list_shellenv		*result;
+
+// 	(void)argc;
+// 	(void)argv;
+// 	ft_baby_yoda_banner();
+// 	setup_signals();
+// 	rl_catch_signals = 0;
+// 	result = (t_list_shellenv *)malloc(sizeof(t_list_shellenv));
+// 	son = malloc(sizeof(t_son));
+// 	if (son == NULL)
+// 		ft_exit("Failed to allocate memory for son", EXIT_FAILURE);
+// 	node = ft_calloc(0, sizeof(t_node));
+// 	if (son == NULL)
+// 		ft_exit("Failed to crate node", EXIT_FAILURE);
+// 	result->env = env_parse(env);
+// 	while (1)
+// 	{
+// 		prompt_active = 0;
+// 		token = ft_calloc(1, sizeof(t_token));
+// 		if (!token)
+// 			ft_exit("Failed to allocate memory for token", EXIT_FAILURE);
+// 		token->read = readline("Minishell>");
+// 		add_history(token->read);
+// 		if (token->read == NULL)
+// 			break ;
+// 		if (token->read[0] == '\0')
+// 			continue ;
+// 		if (token->read == NULL)
+// 			break ;
+// 		node = main_parser(result, token, son);
+// 		if (node == NULL)
+// 			continue ;
+// 		prompt_active = 1;
+// 		execute(son, result, node);
+// 	}
+// 	//ft_free(result, node, NULL, son);
+// 	return (son->code);
+// }
+
